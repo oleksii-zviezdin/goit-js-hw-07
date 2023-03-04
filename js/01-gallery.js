9b7168559b4e5ec2610ec18327922588a1af8455
oleksii-zviezdin <oleksii.zviezdin@gmail.com>
2023-03-04 20:37:52 +0100

    add taks-01

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,46 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryEl = document.querySelector(`.gallery`)
+const imgMarkup = createGallery(galleryItems);
+galleryEl.insertAdjacentHTML('beforeend', imgMarkup)
+
+function createGallery(gallery) {
+    return gallery.map(({ preview, original, description }) => {
+            return `<div class="gallery__item">
+            <a class="gallery__link" href="${original}">
+                <img
+                    class="gallery__image"
+                    src="${preview}"
+                    data-source="${original}"
+                    alt="${description}"
+                />
+            </a>
+        </div>`;
+        })
+        .join('')
+}
+
+galleryEl.addEventListener(`click`, handleClickOnGalleryEl)
+
+function handleClickOnGalleryEl(e) {    
+    e.preventDefault()
+    if (e.target.nodeName !== 'IMG') {
+        return;
+    }
+
+    const instance = basicLightbox.create(`
+    <div class="modal">
+        <img src="${e.target.dataset.source}">
+    </div>
+`)
+    instance.show();
+
+    document.addEventListener(`keydown`, escModal, {once: true})
+    function escModal({code}) {
+        if (code === `Escape`) {
+        instance.close();
+        }
+    }
+}
