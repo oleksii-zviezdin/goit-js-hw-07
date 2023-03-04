9bb188787ed9309946106e0ac569e890f322f6bb
oleksii-zviezdin <oleksii.zviezdin@gmail.com>
2023-03-04 22:03:34 +0100

    add task-02

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,28 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryEl = document.querySelector(`.gallery`)
+const imgMarkup = createGallery(galleryItems);
+galleryEl.insertAdjacentHTML('beforeend', imgMarkup)
+console.log(imgMarkup);
+
+function createGallery(gallery) {
+    return gallery.map(({ preview, original, description }) => {
+            return `<li><a class="gallery__link" href="${original}">
+                <img
+                    class="gallery__image"
+                    src="${preview}"
+                    data-source="${original}"
+                    alt="${description}"
+                />
+            </a>
+            </li>`;
+        })
+        .join('')
+}
+
+const galleryLightBox = new SimpleLightbox(`.gallery a`, {
+    captionsData: `alt`,
+    captionDelay: `250`,
+});
